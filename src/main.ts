2f060bdb8536a05fb6240c5db07be286e71b5b44
Andres Espinosa <andreshx1996-e@hotmail.com>
2026-06-11 12:38:58 -0500

    Configuracion de  app.module.ts y Swagger, creacion de dockler compose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { SwaggerModule } from 'node_modules/@nestjs/swagger/dist/swagger-module';
+import { DocumentBuilder } from 'node_modules/@nestjs/swagger/dist/document-builder';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api'); 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      
+    })
+  ); 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Insurance API')
+    .setDescription('API documentation for the Insurance application')
+    .setVersion('1.0')
+    .addTag('insurance')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  
 }
 bootstrap();
